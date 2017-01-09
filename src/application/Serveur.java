0681b32683bd3f92e7eb278df1b56535629e586b
s133524 <s133524@E-0580.univ-lemans.fr>
2017-01-09 16:40:38 +0100

    serveur_1

--- src/application/Serveur.java
+++ src/application/Serveur.java
@@ -1,5 +1,28 @@
 package application;
 
-public class Serveur {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Serveur 
+{
+	public void main (String[] Args)
+	{
+	try
+	{
+		ServerSocket listener = new ServerSocket(1500);
+		Socket aClient = listener.accept();	
+		// La connexion est établie
+		InputStream in = aClient.getInputStream();
+		BufferedReader din = new BufferedReader(new InputStreamReader (in));
+		System.out.println(din.read());
+		listener.close();
+	}
+	catch (IOException e){}
+	}
 
 }
+ 
