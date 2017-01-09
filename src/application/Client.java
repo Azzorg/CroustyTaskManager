5df7b76f361fe2ae58adf69d5d18d096c8173d9f
s132722 <s132722@E-0579.univ-lemans.fr>
2017-01-09 16:42:02 +0100

    Client v1

--- src/application/Client.java
+++ src/application/Client.java
@@ -1,5 +1,38 @@
 package application;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+public class Client {
+	final public int PORT = 1500;
+	
+	public static void main(String []args){
+		Client client = new Client();
+		
+		
+		try{
+			Socket aClient = new Socket("toto", client.PORT);
+			InputStream in = aClient.getInputStream();
+			OutputStream out = aClient.getOutputStream();
+			out.write(42);;
+			PrintStream pout = new PrintStream(out);
+			pout.println("Hello!");
+			Byte back = (byte) in.read();
+			BufferedReader din = new BufferedReader(new InputStreamReader(in));
+			String response = din.readLine();
+			aClient.close();
+		}
+		catch(UnknownHostException e){
+			System.out.println("Can't find host");
+		}
+		catch(IOException e){
+			System.out.println("Error connecting to host");
+		}
+	}
 }
