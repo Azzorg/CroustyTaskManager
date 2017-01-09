a95227add55bac6a249649515dc644a6b09c7a55
s132722 <s132722@E-0579.univ-lemans.fr>
2017-01-09 17:39:13 +0100

    Serveur-while(true)

--- src/application/Serveur.java
+++ src/application/Serveur.java
@@ -16,26 +16,37 @@ public class Serveur
 	public static void main (String[] Args)
 	{
 		Serveur serveur = new Serveur();
+		ServerSocket listener = null;
 		try
 		{
-			ServerSocket listener = new ServerSocket(serveur.PORT);
-			System.out.println("Attente du client");
-			Socket aClient = listener.accept();	//Attente du client
-			System.out.println("Client" + aClient.getLocalPort() + "accepté");
-			// La connexion est établie
-			InputStream in = aClient.getInputStream();
-			OutputStream out = aClient.getOutputStream();
-			BufferedReader din = new BufferedReader(new InputStreamReader (in));
-			System.out.println(din.readLine());
-			out.write(43);
-			PrintStream pout = new PrintStream(out);
-			pout.println("Goodbye");
-			aClient.close();
-			listener.close();
+			listener = new ServerSocket(serveur.PORT);
+			while(true){
+				System.out.println("Attente du client...");
+				Socket aClient = listener.accept();	//Attente du client
+				System.out.println("Client" + aClient.getLocalPort() + "accepté");
+				// La connexion est établie
+				InputStream in = aClient.getInputStream();
+				OutputStream out = aClient.getOutputStream();
+				BufferedReader din = new BufferedReader(new InputStreamReader (in));
+				System.out.println(din.readLine());
+				out.write(43);
+				PrintStream pout = new PrintStream(out);
+				pout.println("Goodbye");
+				aClient.close();
+			}
 		}
 		catch (IOException e){
 			System.out.println("Error connexion");
 			e.printStackTrace();
+		}
+		finally{
+			try{
+				listener.close();
+			}
+			catch(Exception e){
+				System.out.println(e);
+			}
+			
 		}
 	}
 
