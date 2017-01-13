773cdf0a59f16aecab315623a46736e6ccd6963c
s132722 <s132722@E-0607.univ-lemans.fr>
2017-01-13 15:52:08 +0100

    commit mutli-client v0.1

--- src/application/ActiviteServeur.java
+++ src/application/ActiviteServeur.java
@@ -1,22 +1,35 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.Socket;
 
-public class ActiviteServeur extends Thread{
+public class ActiviteServeur extends Thread {
 	Socket clientSocket;
-	
-	public ActiviteServeur(String n, Socket s){
+
+	public ActiviteServeur(String n, Socket s) {
 		super(n);
 		clientSocket = s;
-		
+		System.out.println("Constructeur activité serveur");
 	}
-	
-	public void Run(){
+
+	public void Run() {
 		System.out.println("Nouveau client");
-	
 		try {
+			System.out.println("Client" + clientSocket.getLocalPort() + "accepté");
+			// La connexion est établie
+			InputStream in = clientSocket.getInputStream();
+			OutputStream out = clientSocket.getOutputStream();
+			BufferedReader din = new BufferedReader(new InputStreamReader(in));
+			System.out.println(din.readLine());
+			out.write(43);
+			PrintStream pout = new PrintStream(out);
+			pout.println("Goodbye");
+
 			clientSocket.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
--- src/application/Serveur.java
+++ src/application/Serveur.java
@@ -1,26 +1,18 @@
 package application;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Vector;
 
 public class Serveur 
 {
 	final public int PORT = 1501;				//Numéro du port utilisé
-	private Vector listClient = new Vector();	//Liste des clients connectés
 	
 	public static void main (String[] Args)
 	{
-		
 		Serveur serveur = new Serveur();
 		ServerSocket welcomeSocket = null;
-		
+		ActiviteServeur thread;
 		
 		try
 		{
@@ -29,20 +21,8 @@ public class Serveur
 				System.out.println("Attente du client...");
 				Socket aClient = welcomeSocket.accept();	//Attente du client
 				
-				new ActiviteServeur("act", aClient).start();
-				
-				/**
-				 * Truc à mettre dans le thread
-				System.out.println("Client" + aClient.getLocalPort() + "accepté");
-				// La connexion est établie
-				InputStream in = aClient.getInputStream();
-				OutputStream out = aClient.getOutputStream();
-				BufferedReader din = new BufferedReader(new InputStreamReader (in));
-				System.out.println(din.readLine());
-				out.write(43);
-				PrintStream pout = new PrintStream(out);
-				pout.println("Goodbye");
-				aClient.close();*/
+				thread = new ActiviteServeur("act", aClient);
+				thread.start();
 			}
 		}
 		catch (IOException e){
